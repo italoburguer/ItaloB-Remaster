be609e481dfc7002f11a803728b0257d2d55e43a
art0rio <eiderestrada@hotmail.com>
2021-09-30 16:35:44 -0500

    first commit

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -7,7 +7,7 @@
 
       <div class="contact-info d-flex align-items-center">
         <i class="bi bi-phone d-flex align-items-center"><span>+57 310 444 55 66</span></i>
-        <i class="bi bi-clock d-flex align-items-center ms-4"><span> Martes a domingo: 3PM - 11PM</span></i>
+        <i class="bi bi-clock d-flex align-items-center ms-4"><span> Martes a domingo: 3PM - 11PM </span></i>
       </div>
 
       <div class="languages d-none d-md-flex align-items-center">
