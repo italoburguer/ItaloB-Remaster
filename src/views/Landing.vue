781d6088cceec4b1e89053826a4a035491019d73
art0rio <eiderestrada@hotmail.com>
2021-10-02 22:09:28 -0500

    menu combos

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -181,7 +181,7 @@
           <div class="col-lg-6 menu-item filter-starters">
             <img src="@/assets/img/menu/lobster-bisque.jpg" class="menu-img" alt="">
             <div class="menu-content">
-              <a href="#">Lobster Bisque</a><span>$5.95</span>
+              <a href="#">Baby beef</a><span>$5.95</span>
             </div>
             <div class="menu-ingredients">
               Lorem, deren, trataro, filede, nerada
@@ -191,7 +191,7 @@
           <div class="col-lg-6 menu-item filter-specialty">
             <img src="@/assets/img/menu/bread-barrel.jpg" class="menu-img" alt="">
             <div class="menu-content">
-              <a href="#">Bread Barrel</a><span>$6.95</span>
+              <a href="#">Calf meat</a><span>$6.95</span>
             </div>
             <div class="menu-ingredients">
               Lorem, deren, trataro, filede, nerada
@@ -201,7 +201,7 @@
           <div class="col-lg-6 menu-item filter-starters">
             <img src="@/assets/img/menu/cake.jpg" class="menu-img" alt="">
             <div class="menu-content">
-              <a href="#">Crab Cake</a><span>$7.95</span>
+              <a href="#">grilled chicken</a><span>$7.95</span>
             </div>
             <div class="menu-ingredients">
               A delicate crab cake served on a toasted roll with lettuce and tartar sauce
@@ -211,7 +211,7 @@
           <div class="col-lg-6 menu-item filter-salads">
             <img src="@/assets/img/menu/caesar.jpg" class="menu-img" alt="">
             <div class="menu-content">
-              <a href="#">Caesar Selections</a><span>$8.95</span>
+              <a href="#">Pork rind</a><span>$8.95</span>
             </div>
             <div class="menu-ingredients">
               Lorem, deren, trataro, filede, nerada
@@ -221,7 +221,7 @@
           <div class="col-lg-6 menu-item filter-specialty">
             <img src="@/assets/img/menu/tuscan-grilled.jpg" class="menu-img" alt="">
             <div class="menu-content">
-              <a href="#">Tuscan Grilled</a><span>$9.95</span>
+              <a href="#">Breaded Chicken</a><span>$9.95</span>
             </div>
             <div class="menu-ingredients">
               Grilled chicken with provolone, artichoke hearts, and roasted red pesto
@@ -231,14 +231,14 @@
           <div class="col-lg-6 menu-item filter-starters">
             <img src="@/assets/img/menu/mozzarella.jpg" class="menu-img" alt="">
             <div class="menu-content">
-              <a href="#">Mozzarella Stick</a><span>$4.95</span>
+              <a href="#">Vegetariana</a><span>$4.95</span>
             </div>
             <div class="menu-ingredients">
               Lorem, deren, trataro, filede, nerada
             </div>
           </div>
 
-          <div class="col-lg-6 menu-item filter-salads">
+          <!-- <div class="col-lg-6 menu-item filter-salads">
             <img src="@/assets/img/menu/greek-salad.jpg" class="menu-img" alt="">
             <div class="menu-content">
               <a href="#">Greek Salad</a><span>$9.95</span>
@@ -246,9 +246,9 @@
             <div class="menu-ingredients">
               Fresh spinach, crisp romaine, tomatoes, and Greek olives
             </div>
-          </div>
+          </div> -->
 
-          <div class="col-lg-6 menu-item filter-salads">
+          <!-- <div class="col-lg-6 menu-item filter-salads">
             <img src="@/assets/img/menu/spinach-salad.jpg" class="menu-img" alt="">
             <div class="menu-content">
               <a href="#">Spinach Salad</a><span>$9.95</span>
@@ -256,9 +256,9 @@
             <div class="menu-ingredients">
               Fresh spinach with mushrooms, hard boiled egg, and warm bacon vinaigrette
             </div>
-          </div>
+          </div> -->
 
-          <div class="col-lg-6 menu-item filter-specialty">
+          <!-- <div class="col-lg-6 menu-item filter-specialty">
             <img src="@/assets/img/menu/lobster-roll.jpg" class="menu-img" alt="">
             <div class="menu-content">
               <a href="#">Lobster Roll</a><span>$12.95</span>
@@ -266,7 +266,7 @@
             <div class="menu-ingredients">
               Plump lobster meat, mayo and crisp lettuce on a toasted bulky roll
             </div>
-          </div>
+          </div> -->
 
         </div>
 
