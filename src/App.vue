67fbfa97adf10b0320c640a8d06caddf75c17b19
art0rio <eiderestrada@hotmail.com>
2021-10-12 10:38:02 -0500

    cambios

--- src/App.vue
+++ src/App.vue
@@ -27,8 +27,8 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  text-align: left;
+  color: #fff;
   margin-top: 60px;
 }
 </style>
